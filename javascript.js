7fbcb134f02df223fc163f8f7152b199300531a4
Azmatul <azmatulhaque2020@gmail.com>
2023-10-18 23:33:29 +0600

    add menu

--- javascript.js
+++ javascript.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menu = document.getElementById("menu");
-    const addMenuItemButton = document.getElementById("addMenuItem");
-
-    // Initial menu items
-    const menuOptions = ["Home", "About", "Services", "Contact"];
-
-    // Function to add a menu item
-    function addMenuItem(text) {
-      const li = document.createElement("li");
-      li.textContent = text;
-      menu.appendChild(li);
-    }
-
-    // Add the initial menu items
-    menuOptions.forEach(function (option) {
-      addMenuItem(option);
-    });
-
-    // Event listener to add a new menu item
-    addMenuItemButton.addEventListener("click", function () {
-      const newItem = prompt("Enter a new menu item:");
-      if (newItem) {
-        addMenuItem(newItem);
-      }
+  
+    // Array of list items with links and images
+    const listItems = [
+      { linkHref: "", imgSrc: "Image/index/dress.png" , text: "Women & Girl Fashion"},
+      { linkHref: "", imgSrc: "Image/index/makeover.png" , text: "Health & Beauty"},
+      { linkHref: "", imgSrc: "Image/index/wristwatch.png" , text: "Watches, Bags, Jewellery"},
+      { linkHref: "", imgSrc: "Image/index/suit.png" , text: "Men's & Boys Fashion"},
+      { linkHref: "", imgSrc: "Image/index/mother.png" , text: "Mother & Baby"},
+      { linkHref: "", imgSrc: "Image/index/responsive.png" , text: "Electronics Devices"},
+      { linkHref: "", imgSrc: "Image/index/tv.png" , text: "TV & Home Appliances"},
+      { linkHref: "", imgSrc: "Image/index/grocery-cart.png" , text: "Groceries"},
+      { linkHref: "", imgSrc: "Image/index/sofa.png" , text: "Home & Lifestyle"},
+    ];
+  
+    listItems.forEach(function (item) {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = item.linkHref;
+      const image = document.createElement("img");
+      image.src = item.imgSrc;
+      const text = document.createTextNode(item.text);
+      image.src = item.imgSrc;
+      link.appendChild(image);
+      listItem.appendChild(link);
+      listItem.appendChild(text);
+      menu.appendChild(listItem);
     });
   });
